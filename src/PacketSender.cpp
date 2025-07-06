25b46cfc6db3d9b5dc4a1eb454e1f16d2fda4362
kiyusama <kiyusama328@gmail.com>
2025-07-06 21:07:44 +0900

    feat: implemented PacketSender

--- src/PacketSender.cpp
+++ src/PacketSender.cpp
@@ -1,3 +1,31 @@
 #include "PacketSender.hpp"
+#include <arpa/inet.h>
+#include <cstring>
+#include <netinet/in.h>
+#include <string>
+#include <sys/socket.h>
+#include <unistd.h>
 
-PacketSender::PacketSender() {}
+const std::string PacketSender::IP = "255.255.255.255";
+PacketSender::PacketSender()
+    : mac{0x60, 0xcf, 0x84, 0xde, 0x6b, 0x09}, packet{} {
+  memset(packet, 0xFF, 6);
+  for (int i = 0; i < 16; ++i) {
+    memcpy(&packet[6 + i * 6], mac, 6);
+  }
+}
+
+void PacketSender::sendWOL() {
+  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  int optval = 1;
+  setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &optval, sizeof(optval));
+
+  sockaddr_in address{};
+  address.sin_family = AF_INET;
+  address.sin_port = htons(PORT);
+  address.sin_addr.s_addr = inet_addr(IP.c_str());
+
+  sendto(sock, packet, sizeof(packet), 0, (sockaddr *)&address,
+         sizeof(address));
+  close(sock);
+}
--- src/PacketSender.hpp
+++ src/PacketSender.hpp
@@ -1,11 +1,19 @@
 #ifndef PACKETSENDER_HPP
 #define PACKETSENDER_HPP
 
+#include <string>
+
 class PacketSender {
 public:
   PacketSender();
+  void sendWOL();
 
 private:
+  unsigned char mac[6];
+  unsigned char packet[102];
+
+  static constexpr int PORT = 9;
+  static const std::string IP;
 };
 
 #endif // !PACKETSENDER_HPP
